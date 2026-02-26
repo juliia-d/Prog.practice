20d3ba91cdc8574d34430b8871660d73939acd3d
juliia-d <yuliadmyshko@gmail.com>
2026-02-26 20:39:58 +0200

    lab1.py

--- lab1.py
+++ lab1.py
@@ -1,51 +1,39 @@
 import argparse
-import sys
-import typer
-from typing import List, Optional
-from pathlib import Path
 
 
-def read_numbers_from_file(prog_py_file):
+def read_numbers_from_file():
     numbers = []
-    with open(prog_py_file, 'r') as f:
+    with open('file', 'r') as f:   
         for line in f:
             line = line.strip()
             if line:
                 numbers.append(int(line))
     return numbers
 
-def write_output(result, prog_py_output=None):
-    if prog_py_output:
-        with open(prog_py_output, 'w') as f:
-            f.write(str(result))
-    else:
-        print(result)
+def write_output(result):
+    with open('output', 'w') as f:  
+        f.write(str(result))
 
 
 def main():
-    args = sys.argv[1:]
+    parser = argparse.ArgumentParser()
+    parser.add_argument("numbers", nargs="*", type=int)
+    parser.add_argument("-f", "--file", action="store_true")
+    parser.add_argument("-o", "--output", action="store_true")
+    args = parser.parse_args()
 
-    numbers = []
-    input_file = None
-    output_file = None
+    total = 0
 
-    i = 0
-    while i < len(args):
-        if args[i] == '-f':
-            input_file = args[i + 1]
-            i += 2
-        elif args[i] == '-o':
-            output_file = args[i + 1]
-            i += 2
-        else:
-            numbers.append(int(args[i]))
-            i += 1
+    if args.file:
+        total = sum(read_numbers_from_file())
+    else:
+        total = sum(args.numbers)
 
-    if input_file:
-        numbers = read_numbers_from_file(input_file)
+    if args.output:
+        write_output(total)
+    else:
+        print(total)
 
-    result = sum(numbers)
-    write_output(result, output_file)
 
 if __name__ == "__main__":
     main()
